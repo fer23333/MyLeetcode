d8a15abe716f9fa9332bbc8950fe1c08fe91f7f3
fer23333 <lz1504@nyu.edu>
2018-09-16 14:27:36 -0400

    Update pre in post order.java

--- Tree/pre in post order.java
+++ Tree/pre in post order.java
@@ -1,7 +1,7 @@
 
-https://www.cnblogs.com/shunyu/p/4986288.html 二叉树遍历分析
-根的意思是输出当前节点值
-preorder 比较简单直接
+// https://www.cnblogs.com/shunyu/p/4986288.html 二叉树遍历分析
+// 根的意思是输出当前节点值
+// preorder 比较简单直接
 
 public List<Integer> preorderTraversal(TreeNode root) {
         if(root == null) return new ArrayList<>();
@@ -20,18 +20,34 @@ public List<Integer> preorderTraversal(TreeNode root) {
         }
         return res;
     }
-inorder 左 根 右
-原则：访问左子树。【先访问左子树中的左子树，再访问左子树中的右子树。】直到访问到叶子结点后输出。
+// inorder 左 根 右
+// 原则：访问左子树。【先访问左子树中的左子树，再访问左子树中的右子树。】直到访问到叶子结点后输出。
 
-　　　　　　　输出根。
+// 　　　　　　　输出根。
 
-　　 访问右子树。【先访问右子树中的左子树，再访问右子树中的右子树。】直到访问到叶子结点后输出。
+// 　　 访问右子树。【先访问右子树中的左子树，再访问右子树中的右子树。】直到访问到叶子结点后输出。
+public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()){
+            while(cur!= null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        
+        return res;
+    }
 
+// postorder
+// 原则：访问左子树。【先访问左子树中的左子树，再访问左子树中的右子树】。直到访问到叶子结点后输出。
 
-postorder
-原则：访问左子树。【先访问左子树中的左子树，再访问左子树中的右子树】。直到访问到叶子结点后输出。
+// 　　　　　　  访问右子树。【先访问右子树中的左子树，再访问右子树中的右子树】。直到访问到叶子结点后输出。
 
-　　　　　　  访问右子树。【先访问右子树中的左子树，再访问右子树中的右子树】。直到访问到叶子结点后输出。
-
-　　　	再返回访问根，并输出。
+// 　　　	再返回访问根，并输出。
 
