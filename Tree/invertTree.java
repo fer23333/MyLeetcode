3b9d374bc44e52223fa4c0f7730fa021f0a19777
fer23333 <lz1504@nyu.edu>
2018-09-16 17:24:43 -0400

    Update invertTree.java

--- Tree/invertTree.java
+++ Tree/invertTree.java
@@ -26,3 +26,20 @@ public TreeNode invertTree(TreeNode root) {
     }
 
 //level order 不需要size控制的bfs
+public TreeNode invertTree(TreeNode root) {
+        if(root == null) return root;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode cur = q.poll();
+            TreeNode temp = cur.left;
+            cur.left = cur.right;
+            cur.right = temp;
+            if(cur.left != null)
+                q.offer(cur.left);
+            if(cur.right != null)
+                q.offer(cur.right);
+            
+        }
+        return root;
+    }
