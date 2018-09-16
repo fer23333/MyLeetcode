ac2b11f89dfdf0a47c7e4dbe6b81e85fdf98d8ca
fer23333 <lz1504@nyu.edu>
2018-09-16 15:10:41 -0400

    Update level order.java

--- Tree/level order.java
+++ Tree/level order.java
@@ -1,1 +1,27 @@
 
+//BFS
+//
+public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null) return result;
+        queue.offer(root);
+        
+        while(!queue.isEmpty()){
+            List<Integer> cur = new ArrayList<>();
+            int size = queue.size();    
+            for(int i = 0; i< size; i++){
+                TreeNode head = queue.poll();
+                cur.add(head.val);
+                if(head.left !=null){
+                    queue.offer(head.left);
+                }
+                if(head.right != null){
+                    queue.offer(head.right);
+                }
+            }
+            result.add(cur);   
+        }
+    return result;
+    } 
