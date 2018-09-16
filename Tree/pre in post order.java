5330d3d053042e12edeff3945100adcdbb2343ad
fer23333 <lz1504@nyu.edu>
2018-09-16 14:52:49 -0400

    Update pre in post order.java

--- Tree/pre in post order.java
+++ Tree/pre in post order.java
@@ -2,7 +2,8 @@
 // https://www.cnblogs.com/shunyu/p/4986288.html 二叉树遍历分析
 // 根的意思是输出当前节点值
 // preorder 比较简单直接
-
+//time O(n) space O(h) height 如果是balanced O(height) O(logn) 如果不是平衡的 worst case 是O(n)
+注意堆栈 先右后左 
 public List<Integer> preorderTraversal(TreeNode root) {
         if(root == null) return new ArrayList<>();
         Deque<TreeNode> stack = new ArrayDeque<>();
@@ -22,12 +23,12 @@ public List<Integer> preorderTraversal(TreeNode root) {
     }
 // inorder 左 根 右
 // 原则：访问左子树。【先访问左子树中的左子树，再访问左子树中的右子树。】直到访问到叶子结点
-
-// 　　　　　　　输出根。
-
+// 　　　输出根。
 // 　　 访问右子树。【先访问右子树中的左子树，再访问右子树中的右子树。】直到访问到叶子结点
 
 //  一直往左找到叶子节点输出 再右移一个 再往复
+// for each node, travel to its left child until it's left leaf, then pop to left leaf's higher level node A, 
+// and switch to A's right branch. Keep the above steps until cur is null and stack is empty
 public List<Integer> inorderTraversal(TreeNode root) {
         if(root == null) return new ArrayList<>();
         List<Integer> res = new ArrayList<>();
@@ -48,10 +49,8 @@ public List<Integer> inorderTraversal(TreeNode root) {
 
 // postorder
 // 原则：访问左子树。【先访问左子树中的左子树，再访问左子树中的右子树】。直到访问到叶子结点
-
 // 　　　访问右子树。【先访问右子树中的左子树，再访问右子树中的右子树】。直到访问到叶子结点
-
-// 　　　	再返回访问根，并输出。
+// 　　　再返回访问根，并输出。
 public List<Integer> postorderTraversal(TreeNode root) {
         //non-recursive;
         Stack<TreeNode> stack = new Stack<TreeNode>();
