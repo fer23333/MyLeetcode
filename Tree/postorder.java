74fe68147dcd2a223a40fdd6c4cec7a2e18bcfe9
fer23333 <lz1504@nyu.edu>
2018-09-16 16:21:10 -0400

    Create postorder.java

---
+++ Tree/postorder.java
@@ -0,0 +1,33 @@
+//两种形式  
+public List<Integer> postorderTraversal(TreeNode root) {
+            //traversal;
+        List<Integer> result = new ArrayList<Integer>();
+        traverse(result, root);
+        return result;
+    }    
+    public void traverse (List<Integer> result, TreeNode root){
+        if(root == null){
+            return; 
+        }
+        traverse(result, root.left);
+        traverse(result, root.right);
+        result.add(root.val);
+    }
+
+  //1.一个和全局变量, 一个和子模块有关
+ //2.从下往上走 会有层数相关
+ public List<Integer> postorderTraversal(TreeNode root) {
+        //divide & conquer;
+        List<Integer> result = new ArrayList<Integer>();
+        if(root == null){
+            return result;
+        }
+        
+        List<Integer> leftNode = postorderTraversal(root.left);
+        List<Integer> rightNode = postorderTraversal(root.right);
+        
+        result.addAll(leftNode);
+        result.addAll(rightNode);
+        result.add(root.val);
+        return result;
+    }
