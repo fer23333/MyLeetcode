5a9e86058e2d4eda43015197b0c38d311917429b
fer <lz1504@nyu.edu>
2018-09-30 20:06:23 -0400

    Update Serialize and Deserialize Binary Tree.java

--- Tree/Serialize and Deserialize Binary Tree.java
+++ Tree/Serialize and Deserialize Binary Tree.java
@@ -1,23 +1,35 @@
+package com.company;
+
+import java.util.*;
+
 class TreeNode {
-     int val;
+    int val;
     TreeNode left;
-     TreeNode right;
-     TreeNode(int x) { val = x; }
- }
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
 
-public class Codec{
-    public static void main(String[] args){
-         Codec codec = new Codec();
-         codec.deserialize(codec.serialize(root));
 
-    }
-    // Encodes a tree to a single string.
+class Codec{
+//    public static void main(String[] args) {
+//        // write your code here
+//        TreeNode root = new TreeNode(1);
+//        TreeNode t1 = new TreeNode(2);
+//        TreeNode t2 = new TreeNode(3);
+//        root.left = t1;root.right = t2;
+//        TreeNode t3 = new TreeNode(4);
+//        TreeNode t4  = new TreeNode(5);
+//        t2.left = t3; t2.right = t4;
+//
+//        Codec codec = new Codec();
+//        codec.deserialize(codec.serialize(root));
+//    }
     public String serialize(TreeNode root) {
         if(root == null) return "null";
         StringBuilder sb = new StringBuilder();
         helperS(root, sb);
-        System.out.println(sb.toString());
         sb.deleteCharAt(sb.length() - 1);
+        System.out.println(sb.toString());
         return(sb.toString());
     }
     public static void helperS(TreeNode root, StringBuilder sb){
@@ -26,8 +38,8 @@ public class Codec{
             return;
         }
         sb.append(root.val+ ",");
-        helper(root.left, sb);
-        helper(root.right, sb);
+        helperS(root.left, sb);
+        helperS(root.right, sb);
     }
 
     // Decodes your encoded data to tree.
@@ -42,10 +54,12 @@ public class Codec{
     public static TreeNode helperD(Deque<String> nodes){
         String val =  nodes.poll();
         if(val.equals("null")) return null;
-        else
+        else{
             TreeNode node =  new TreeNode(Integer.valueOf(val));
             node.left = helperD(nodes);
             node.right = helperD(nodes);
             return node;
+        }
+
     }
 }
