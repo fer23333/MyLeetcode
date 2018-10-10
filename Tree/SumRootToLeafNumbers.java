22317d19781cb87522a1164b36f763e2ee69d430
fer <lz1504@nyu.edu>
2018-10-10 16:19:03 -0400

    Update SumRootToLeafNumbers.java

--- Tree/SumRootToLeafNumbers.java
+++ Tree/SumRootToLeafNumbers.java
@@ -21,3 +21,36 @@
         helper(root.right, res, sb);
         sb.deleteCharAt(sb.length() -1);
     }
+
+//改进 
+    int total;
+    
+    public int sumNumbers(TreeNode root) {
+        total = 0;
+        helper(root, 0);
+        return total;
+    }
+    
+    void helper(TreeNode root, int sum) {
+        if (root == null) return;
+        
+        sum = sum * 10 + root.val;
+        
+        if (root.left == null && root.right == null) {
+            total += sum;
+            return;
+        }
+        
+        helper(root.left, sum);
+        helper(root.right, sum);
+    }
+//去除全局变量
+public int sumNumbers(TreeNode root) {
+	return sum(root, 0);
+}
+
+public int sum(TreeNode n, int s){
+	if (n == null) return 0;
+	if (n.right == null && n.left == null) return s*10 + n.val;
+	return sum(n.left, s*10 + n.val) + sum(n.right, s*10 + n.val);
+}
