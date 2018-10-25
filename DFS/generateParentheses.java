da79e12ba748756a379bb909f0eba996b3ad3903
fer <lz1504@nyu.edu>
2018-10-25 19:28:59 -0400

    Create generateParentheses.java

---
+++ DFS/generateParentheses.java
@@ -0,0 +1,21 @@
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        generateOneByOne("", list, n, n);
+        return list;
+    }
+    public void generateOneByOne(String sublist, List<String> list, int left, int right){
+        if(left > 0){
+            System.out.println(sublist);
+            generateOneByOne( sublist+ "(", list, left-1, right);
+        }
+        if(left < right){
+            System.out.println(sublist);
+            generateOneByOne(sublist+ ")" , list, left, right-1);
+        }
+        if(left == 0 && right == 0){
+            System.out.println(sublist);
+            list.add(sublist);
+            return;
+        }
+
+    }
