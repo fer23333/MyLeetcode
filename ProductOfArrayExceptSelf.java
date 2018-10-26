a8c22cc3984a6a0a6df61f7cc65c7c9b64570029
fer <lz1504@nyu.edu>
2018-10-25 21:15:16 -0400

    Create ProductOfArrayExceptSelf.java

---
+++ ProductOfArrayExceptSelf.java
@@ -0,0 +1,15 @@
+    public int[] productExceptSelf(int[] nums) {
+        if(nums == null || nums.length == 0) return nums;
+        int[] res = new int[nums.length];
+        int n = nums.length;
+        res[0]= 1;
+        for(int i =1; i< n; i++){
+            res[i] = res[i-1] * nums[i-1]; 
+        }
+        int temp =1;
+        for(int i = n-1 ; i>=0; i--){
+            res[i] *= temp;
+            temp *= nums[i];
+        }
+        return res;
+    }
