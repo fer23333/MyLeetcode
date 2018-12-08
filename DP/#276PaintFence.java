4da36d576ce6fea246e15da3bb2d2db2c2d11179
fer <lz1504@nyu.edu>
2018-12-08 17:59:52 -0500

    Update #276PaintFence.java

--- DP/#276PaintFence.java
+++ DP/#276PaintFence.java
@@ -27,3 +27,14 @@
         }
         return dp[3];
     }
+
+    int numWays(int n, int k) {
+        if (n == 0) return 0;
+        int same = 0, diff = k;
+        for (int i = 2; i <= n; ++i) {
+            int t = diff;
+            diff = (same + diff) * (k - 1);
+            same = t;
+        }
+        return same + diff;
+    }
