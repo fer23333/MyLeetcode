bd8d32a448b1d75d001d251595f7694dad27f213
fer <lz1504@nyu.edu>
2019-01-11 15:34:11 -0500

    Update #819MostCommonWord.java

--- Amazon/#819MostCommonWord.java
+++ Amazon/#819MostCommonWord.java
@@ -1,4 +1,7 @@
- public String mostCommonWord(String paragraph, String[] banned) {
+priorityqueue 构建时间复杂度为O(n)不是O(nlogn) 因为有一半不需要logn操作
+每次offer和poll时由于堆得heapify操作 是O(logn)
+
+public String mostCommonWord(String paragraph, String[] banned) {
         String s = paragraph.replaceAll("[^\\w+]", " ").toLowerCase();
         //    ^[//w+]表示 [^A-za-z0-9] 的word 并且包含多个word的情况
         String[] str = s.split("\\s+");
