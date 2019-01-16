fb4d16affcc6a02e0c7d026a06cf0d2ac1a54e7e
fer <lz1504@nyu.edu>
2019-01-16 18:29:11 -0500

    Create SortColors及其变种.java

---
+++ Google/SortColors及其变种.java
@@ -0,0 +1,14 @@
+sort color: 0 1 2
+Google : <x  x-y   >y
+
+counting sort
+
+分成3个部分有一次做完的方法
+
+
+
+Based on Comparison
+
+A:quick sort, merge sort, bubble sort, insertion sort 给予比较的排序，最快O(nlogn)
+
+B:counting sort, radix sort, bucket sort O(n)
