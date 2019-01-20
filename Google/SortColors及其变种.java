ca4c1706a3349f240904c08de36842eb630491d9
fer <lz1504@nyu.edu>
2019-01-19 23:08:14 -0500

    Update SortColors及其变种.java

--- Google/SortColors及其变种.java
+++ Google/SortColors及其变种.java
@@ -4,7 +4,31 @@ Google : <x  x-y   >y
 counting sort
 
 分成3个部分有一次做完的方法
+    public void sortColors(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return;
+        }
+//         使用一次扫描的办法。
+// 设立三根指针，left, index, right。定义如下规则：
 
+// left 的左侧都是 0（不含 left）
+// right 的右侧都是 2（不含 right）
+// index 从左到右扫描每个数，如果碰到 0 就丢给 left，碰到 2 就丢给 right。碰到 1 就跳过不管。
+//         000   xl   1  0 1  2    xr   222
+//              left     i         right
+//由于right所在位置可为任何数 而他的右侧都为2 所以交换后不能变 index 还需第二次判断
+//20211210
+        int left =0 ; int right = nums.length - 1;
+        int index = 0;
+        while(index <= right){
+            if(nums[index] == 1){
+                index++;
+            }else if(nums[index] == 0){
+                swap(nums, left++, index++);
+            }else if (nums[index] == 2){
+                swap(nums, right--, index);
+            }
+        }
 
 
 Based on Comparison
