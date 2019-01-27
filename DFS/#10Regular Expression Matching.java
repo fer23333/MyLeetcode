087d6edc6eb3986ab7976d7e6133d07cce1d07bf
fer <lz1504@nyu.edu>
2019-01-26 23:59:51 -0500

    Create #10Regular Expression Matching.java

---
+++ DFS/#10Regular Expression Matching.java
@@ -0,0 +1,13 @@
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "a*") → true
+isMatch("aa", ".*") → true
+isMatch("ab", ".*") → true
+isMatch("aab", "c*a*b") → true
+
+
+
