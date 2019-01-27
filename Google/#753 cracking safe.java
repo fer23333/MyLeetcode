9e37cbf03c7e4e8d1347fe5a3715cc53fe1850dd
fer <lz1504@nyu.edu>
2019-01-26 23:18:20 -0500

    Update #753 cracking safe.java

--- Google/#753 cracking safe.java
+++ Google/#753 cracking safe.java
@@ -1,5 +1,7 @@
 汉密尔顿环 np-complete问题
+de Bruijn序列可以通过在k个符号上采用n维de Bruijn图的哈密​​顿路径（或等效地，（n  -1）维de Bruijn图的欧拉循环）来构造
 https://www.youtube.com/watch?v=iPLQgXUiU14
+https://en.wikipedia.org/wiki/De_Bruijn_sequence#Uses
 
 // n=1, k=2 01  0 1   -> 01
 // n=2, k=2 01  00 01 10 11  ->00110
