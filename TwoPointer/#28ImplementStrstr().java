e2a73b6210bf259c7cd3f4c5f7c9594debdab608
fer <lz1504@nyu.edu>
2019-01-28 17:18:48 -0500

    Update #28ImplementStrstr().java

--- TwoPointer/#28ImplementStrstr().java
+++ TwoPointer/#28ImplementStrstr().java
@@ -1,4 +1,5 @@
     注意for循环的写法
+    kmp可以优化到o(n)
         
    String indexOf(String str) : This method returns the index 
    within this string of the first occurrence of the specified substring. If it does not occur as a substring, -1 is returned.
@@ -21,4 +22,24 @@
             }
         }
         return -1;
+    }
+
+// more elegant
+    public int strStr(String haystack, String needle) {
+        if(needle == null || needle.length() ==0 || needle.trim().equals("")){
+            return 0; 
+        }      
+        for(int i =0; ;i++){
+            for(int j =0; ; j++){
+                if(j == needle.length()){
+                    return i;
+                }
+                if(i+j == haystack.length()){
+                    return -1;
+                }
+                if(haystack.charAt(i+j) != needle.charAt(j)){
+                    break;  
+                }
+            }      
+        }
     }
