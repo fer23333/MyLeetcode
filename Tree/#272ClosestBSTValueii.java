11260388009b2648fb3cfbeec72be18de86204ea
fer <lz1504@nyu.edu>
2019-02-03 18:23:25 -0500

    Update #272ClosestBSTValueii.java

--- Tree/#272ClosestBSTValueii.java
+++ Tree/#272ClosestBSTValueii.java
@@ -32,3 +32,55 @@ O(n), O(n)
 从指定节点开始分别向前和向后遍历，直到找到k个最接近target的节点。
 使用prev和next两个栈分别记录前驱和后继，goPrev相当于反向中序遍历，goNext相当于正向中序遍历。
 
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        Stack<TreeNode> next = new Stack<TreeNode>();
+        Stack<TreeNode> prev = new Stack<TreeNode>();
+        TreeNode node = root;
+        
+        // find the nodes closest to target
+        while (node != null) {
+            if (node.val < target) {
+                prev.push(node);
+                node = node.right;
+            } else {
+                next.push(node);
+                node = node.left;
+            }
+        }
+        
+        List<Integer> ret = new LinkedList<Integer>();
+        
+        while (ret.size() < k) {
+            double distp = prev.isEmpty() ? Integer.MAX_VALUE : Math.abs(prev.peek().val - target);
+            double distn = next.isEmpty() ? Integer.MAX_VALUE : Math.abs(next.peek().val - target);
+            
+            // compare and find the closest node, and move the corresponding stack.
+            if (distp < distn) {
+                ret.add(0, prev.peek().val);
+                goPrev(prev);
+            } else {
+                ret.add(next.peek().val);
+                goNext(next);
+            }
+        }
+        
+        return ret;
+    }
+    
+    private void goNext(Stack<TreeNode> st) {
+        TreeNode r = st.pop().right;
+        
+        while (r != null) {
+            st.push(r);
+            r = r.left;
+        }
+    }
+    
+    private void goPrev(Stack<TreeNode> st) {
+        TreeNode l = st.pop().left;
+        
+        while (l != null) {
+            st.push(l);
+            l = l.right;
+        }
+    }
