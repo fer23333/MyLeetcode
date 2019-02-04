5ea6c47c66df5b1a7c56a42e4005ae83f3b8890d
fer <lz1504@nyu.edu>
2019-02-03 23:50:38 -0500

    Create #53MaximumSubarray.java

---
+++ DP/#53MaximumSubarray.java
@@ -0,0 +1,25 @@
+使用prefixsum 
+sum += nums[i] (sum[i]- sum[i-1] =nums[i])
+
+可变为买卖股票i的题
+
+follow up maximum submatrix
+
+Given an n x n matrix of positive and negative integers, find the submatrix with the largest possible sum
+Given matrix = 
+[
+[1,3,-1],
+[2,3,-2],
+[-1,-2,-3]
+]
+return 9.
+Explanation:
+the submatrix with the largest possible sum is:
+[
+[1,3],
+[2,3]
+]
+
+
+submatrix sum
+找到和为0的submatrix
