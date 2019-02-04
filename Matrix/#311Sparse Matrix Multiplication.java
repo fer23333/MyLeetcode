d3af1066e97969a7128366d427548032fe279c38
fer <lz1504@nyu.edu>
2019-02-03 23:17:02 -0500

    Update #311Sparse Matrix Multiplication.java

--- Matrix/#311Sparse Matrix Multiplication.java
+++ Matrix/#311Sparse Matrix Multiplication.java
@@ -1,1 +1,54 @@
 
+a m*n
+b n*k
+矩阵直接相乘计算，时间复杂度O(m*n*k)
+  
+稀疏矩阵 有k个非零元素 跳过0
+    public int[][] multiply(int[][] A, int[][] B) {
+        // Write your code here
+        int n = A.length;
+        int m = B[0].length;
+        int t = A[0].length;
+        int[][] C = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < t; k++) {
+                if (A[i][k] == 0) {
+                    continue;
+                }
+                for (int j = 0; j < m; j++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return C;
+    }
+把A，B都转化为行向量和列向量O（n2）
+    public int[][] multiply(int[][] A, int[][] B) {
+        // Write your code here
+        int n = A.length;
+        int m = B[0].length;
+        int t = A[0].length;
+        int[][] C = new int[n][m];
+
+        List<List<Integer>> col = new ArrayList<>();
+        for (int i = 0; i < t; i++) {
+            col.add(new ArrayList<>());
+            for (int j = 0; j < m; j++) {
+                if (B[i][j] != 0) {
+                    col.get(i).add(j);
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < t; k++) {
+                if (A[i][k] == 0) {
+                    continue;
+                }
+                for (int j: col.get(k)) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return C;
+    }
