b5e9847d9997951951548b16e545e5b187d71049
fer <lz1504@nyu.edu>
2019-02-04 14:33:14 -0500

    Update and rename BinaryTreePaths.java to #257BinaryTreePaths.java

--- Tree/#257BinaryTreePaths.java
+++ Tree/#257BinaryTreePaths.java
@@ -20,3 +20,28 @@ public List<String> binaryTreePaths(TreeNode root) {
             helper(root.right, result, n+"->" + root.right.val);
         
     }
+
+//divide and conquer
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+        if (root == null) {
+            return paths;
+        }
+        
+        List<String> leftPaths = binaryTreePaths(root.left);
+        List<String> rightPaths = binaryTreePaths(root.right);
+        for (String path : leftPaths) {
+            paths.add(root.val + "->" + path);
+        }
+        for (String path : rightPaths) {
+            paths.add(root.val + "->" + path);
+        }
+        
+        // root is a leaf
+        if (paths.size() == 0) {
+            paths.add("" + root.val);
+        }
+        
+        return paths;
+    }
