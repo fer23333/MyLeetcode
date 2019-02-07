9f686e2fd0b4d4937f4383df414521107ef86c9b
fer <lz1504@nyu.edu>
2019-02-07 00:48:14 -0500

    Update #307. Range Sum Query - Mutable.java

--- BinarySegementTree/#307. Range Sum Query - Mutable.java
+++ BinarySegementTree/#307. Range Sum Query - Mutable.java
@@ -11,107 +11,89 @@ Segment Tree can be broken down to the three following steps:
 1.Pre-processing step which builds the segment tree from a given array.
 2.Update the segment tree when an element is modified.
 3.Calculate the Range Sum Query using the segment tree.
-
-class SegmentTreeNode {
-    SegmentTreeNode left, right;
-    int sum, start, end;
-    
-    public SegmentTreeNode(int start, int end, int sum) {
-        this.start = start;
-        this.end = end;
-        this.sum = sum;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-class SegmentTree {
-    SegmentTreeNode root;
-    
-    public SegmentTree(int[] nums) {
-        root = build(0, nums.length - 1, nums);
-    }
-    
-    public SegmentTreeNode build(int start, int end, int[] A) {
-        // write your code here
-        if(start > end) { 
-            return null;
-        }
-        
-        SegmentTreeNode root = new SegmentTreeNode(start, end, 0);
-        
-        if(start != end) {
-            int mid = (start + end) / 2;
-            root.left = build(start, mid, A);
-            root.right = build(mid+1, end, A);
-            root.sum = root.left.sum + root.right.sum;
-        } else {
-            root.sum =  A[start];
-            
-        }
-        return root;
-    }
-    
-    public int querySegmentTree(SegmentTreeNode root, int start, int end) {
-        // write your code here
-
-        if(start == root.start && root.end == end) { 
-            return root.sum;
-        }
-        
-        int mid = (root.start + root.end)/2;
-        int leftsum = 0, rightsum = 0;
-        
-        if(start <= mid) {
-            if( mid < end) { 
-                leftsum =  querySegmentTree(root.left, start, mid);
-            } else { 
-                leftsum = querySegmentTree(root.left, start, end);
-            }
-        }
-        
-        if(mid < end) { 
-            if(start <= mid) {
-                rightsum = querySegmentTree(root.right, mid+1, end);
-            } else {
-                rightsum = querySegmentTree(root.right, start, end);
-            } 
-        }  
-        
-        return leftsum + rightsum;
-    }
-    public void modifySegmentTree(SegmentTreeNode root, int index, int value) {
-        // write your code here
-        if(root.start == index && root.end == index) {
-            root.sum = value;
-            return;
-        }
-        
-        int mid = (root.start + root.end) / 2;
-        if(root.start <= index && index <=mid) {
-            modifySegmentTree(root.left, index, value);
-        }
-        
-        if(mid < index && index <= root.end) {
-            modifySegmentTree(root.right, index, value);
-        }
-        
-        root.sum = root.left.sum + root.right.sum;
-    }
-}
-
+//使用树的方法建立 来自九章答案 代码很长
+  
+// 使用数组的方法建立
+  
 class NumArray {
-    SegmentTree st;
 
+    //    0-5 2-5 0-1 2-3 4-5    1 2 4 3 5 7
+    //  0 1    2   3   4  5      0 1 2 3 4 5  index
+    //                           6 7 8 9 10 11
+    //              '22'
+    //      '7'           '15'  
+    //  '3'   4      '8'      7
+    //1     2       3  5 
+    private int[] tree;
+    private int size;
     public NumArray(int[] nums) {
-        st = new SegmentTree(nums);
+        if(nums.length > 0){
+            size = nums.length;
+            tree = new int[2*size];
+            buildTree(nums);
+        }
     }
     
     public void update(int i, int val) {
-        st.modifySegmentTree(st.root, i, val);
+        i += size;
+        tree[i] = val;
+        while(i>0){
+            //从底至上更新节点
+            int left = i;
+            int right = i;
+            if(i % 2 ==0){
+                //为偶数 更改的节点为左子树
+                right = i+1;
+                //那么他的右边为i+1
+            }else{
+                left = i-1;
+            }
+            tree[i/2]= tree[left] + tree[right];
+            //更新父节点
+            i /=2;
+        }
     }
     
     public int sumRange(int i, int j) {
-        return st.querySegmentTree(st.root, i, j);
+        i += size;
+        j += size;
+        int sum =0;
+        //fix成i在左子树 j在右子树的结构
+        while(i <= j){
+            if(i % 2 != 0){
+                //i不在左子树 在右子树上
+                sum += tree[i];
+                i++;
+                //加上当前的值 并往右走一步到左子树
+            }
+            if(j % 2 == 0){
+                //j不在右子树  在左子树上
+                sum+= tree[j];
+                j--;
+                //走一步到右子树
+            }
+            i/=2;
+            j/=2;
+        }
+        return sum;
+        
     }
+    
+    public void buildTree(int[] nums){
+        for(int i=size, j=0; i<2*size; i++,j++){
+            tree[i]= nums[j];
+        }
+        for(int i=size-1; i>=0; i--){
+            tree[i]= tree[2*i]+ tree[2*i+1];
+        }
+    }
+    
+
 }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
