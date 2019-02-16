ce11259f957f5c8b86ad7c3e0cca0e2eeab48c21
fer <lz1504@nyu.edu>
2019-02-16 18:18:45 -0500

    Update #684. Redundant Connection.java

--- Google/#684. Redundant Connection.java
+++ Google/#684. Redundant Connection.java
@@ -8,9 +8,83 @@ https://blog.csdn.net/dm_vincent/article/details/7655764
 
 Path compression involves changing the x = parent[x] in the find function to parent[x] = find(parent[x]). 
 Basically, as we compute the correct leader for x, we should remember our calculation.
-
+ O(1)
+    public int find(int x){
+        if(parent[x] == x){
+           return x;
+        } 
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
 Union-by-rank involves distributing the workload of find across leaders evenly. 
-  Whenever we dsu.union(x, y), we have two leaders xr, yr and we have to choose whether we want parent[x] = yr or parent[y] = xr. 
-  We choose the leader that has a higher following to pick up a new follower.
-Specifically, the meaning of rank is that there are less than 2 ^ rank[x] followers of x. 
-  This strategy can be shown to give us better bounds for how long the recursive loop in dsu.find could run for.
+The above operations can be optimized to O(Log n) in worst case. 
+The idea is to always attach smaller depth tree under the root of the deeper tree.
+   //若不采用union by rank 复杂度O(n)
+ public void union(int a, int b){
+        int pa = find(a);
+        int pb = find(b);
+        if(pa != pb){
+            parent[pb] = pa;
+        }    
+    }
+
+//code
+class UnionFind{
+    private int[] parent;
+    public UnionFind(int n){
+        parent = new int[n+1];
+        for(int i=1 ;i <= n; i++ ){
+            parent[i]= i;
+        }
+    }
+    public void union(int a, int b){
+        int pa = find(a);
+        int pb = find(b);
+        if(pa != pb){
+            parent[pb] = pa;
+        }
+        
+    }
+    public int find(int x){
+        if(parent[x] == x){
+           return x;
+        } 
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
+}
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        UnionFind uf = new UnionFind(n);
+        int[] res= new int[2];
+        for(int[] edge : edges){
+            if(uf.find(edge[0]) == uf.find(edge[1])){
+                res[0] = edge[0];
+                res[1] = edge[1];
+                break;
+            }else{
+                uf.union(edge[0], edge[1]);
+            }
+        }
+        return res;
+    }
+}
+
+
+//采用优化后的 union
+    public boolean union(int a, int b){
+        int pa = find(a);
+        int pb = find(b);
+        if(pa == pb){
+            return false;
+        }else if(rank[pa] > rank[pb]){
+            parent[pa] = pb;
+        }else if(rank[pa] < rank[pb]){
+            parent[pb] = pa;
+        }else{
+           parent[pa] = pb;
+            rank[pb]++;
+        }
+        return true;
+    }
