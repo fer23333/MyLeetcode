392d490432f6ead46a525d13b0eb1ce36243fdff
fer <lz1504@nyu.edu>
2019-03-01 17:32:31 -0500

    Update #146 LRU Cache.java

--- Amazon/#146 LRU Cache.java
+++ Amazon/#146 LRU Cache.java
@@ -1,1 +1,32 @@
 
+import java.util.LinkedHashMap;
+
+class LRUCache {
+    private LinkedHashMap<Integer, Integer> map;
+    private final int CAPACITY;
+    public LRUCache(int capacity) {
+        CAPACITY = capacity;
+        map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true){
+          //0.75 负载因子 load factor 
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > CAPACITY;
+            }
+        };
+    }
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+
+
+使用doublylinkedlist 和 hashmap
