8f60a4d6d43fe17c78ecdec143680011b92ab6e6
fer <lz1504@nyu.edu>
2019-03-03 15:05:12 -0500

    Update #819MostCommonWord.java

--- Amazon/#819MostCommonWord.java
+++ Amazon/#819MostCommonWord.java
@@ -1,3 +1,14 @@
 priorityqueue 构建时间复杂度为O(n)不是O(nlogn) 因为有一半不需要logn操作
 每次offer和poll时由于堆得heapify操作 是O(logn)
-
+  
+      public String mostCommonWord(String paragraph, String[] banned) {
+        String[] words = paragraph.toLowerCase().split("[ !?',;.]+");
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String word : words) map.put(word, map.getOrDefault(word, 0) + 1);
+        for(String word : banned) if(map.containsKey(word)) map.remove(word);
+        String res = null;
+        for(String word : map.keySet())
+            if(res == null || map.get(word) > map.get(res))
+                res = word;
+        return res;
+    }
