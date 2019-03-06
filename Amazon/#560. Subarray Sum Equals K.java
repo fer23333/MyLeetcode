4bc43d1daa2b1155f8782f7f3b39331fc808d0a1
fer <lz1504@nyu.edu>
2019-03-05 19:10:08 -0500

    Update #560. Subarray Sum Equals K.java

--- Amazon/#560. Subarray Sum Equals K.java
+++ Amazon/#560. Subarray Sum Equals K.java
@@ -1,25 +1,36 @@
- //我的解法 O(n2)
+ //prefixSum O(n2)
  public int subarraySum(int[] nums, int k) {
         if(nums == null || nums.length  == 0){
             return 0;
         }
         int count =0;
         for(int i =0; i< nums.length; i++){
-            int sum = nums[i];
+            int prefixSum = 0;
             for(int j = i; j< nums.length; j++){
-                if(j == i){
-                    if(sum == k){
-                        count++;
-                    }
-                }else{
-                    sum += nums[j];
-                    if(sum == k){
-                        count++;
-                    }
+                prefixSum += nums[j];
+                if(prefixSum == k){
+                  count++;
                 }
             }
         }
         return count;
     }
-    
-    //using prefixSum O(n2)
+  // prefixSum + hashmap
+ // the sums between i and j is presum[i,j] = pre_sum[j] - pre_sum[i-1]. And our goal is to make pre_sum[j] - pre_sum[i-1] = k
+//存贮 出现次数 解决 0,0,0 重复数字
+    public int subarraySum(int[] nums, int k) {
+        int count = 0; 
+        int prefixSum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+     //初始化为0,1 解决 若有个数为k 这也是个情况
+        for(int i = 0; i< nums.length; i++){
+            prefixSum += nums[i];
+            if(map.containsKey(prefixSum - k)){
+                count += map.get(prefixSum - k);
+            }
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+        return count;
+    }
+
