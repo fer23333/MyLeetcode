09875774363b8ca2aaf27712cb0ca6067ae10a35
fer <lz1504@nyu.edu>
2019-03-07 15:31:40 -0500

    Update 刷题笔记.java

--- Amazon/刷题笔记.java
+++ Amazon/刷题笔记.java
@@ -5,7 +5,9 @@ Largest Rectangle in Histogram 也可以单调栈 tbd
 
 #5longest palidrome substring  从中间向两边扩展 并取 low index 和 maxLen 最后返回substring  ababb -> aba 或者 bab
 
-#3longest substring without repeating characets abcabcbb -> abc
+#3longest substring without repeating characets abcabcbb -> abc sliding window 使用set记录 或者 map记录 index 再找 最近的重复数字 往后一位继续
+
+
 
 
 
