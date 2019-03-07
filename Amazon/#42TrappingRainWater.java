5d5f4c4703dd38b591ebe53058714099f49d5a0c
fer <lz1504@nyu.edu>
2019-03-06 21:52:26 -0500

    Update #42TrappingRainWater.java

--- Amazon/#42TrappingRainWater.java
+++ Amazon/#42TrappingRainWater.java
@@ -31,4 +31,27 @@
     }
     
 //双指针 timeO(n), space O(1)
-//
+//原理和 container with most water 的木桶原理相似
+//设立leftmax rightmax两坐墙 然后移动短的一边 对leftmax 和left 或者 rightmax 和right 做差值 
+    public int trap(int[] height) {
+        int a=0;
+        int b=height.length-1;
+        int max=0;
+        int leftmax=0;
+        int rightmax=0;
+            //a<= b 也可以
+        while(a< b){
+            leftmax=Math.max(leftmax,height[a]);
+            rightmax=Math.max(rightmax,height[b]);
+            if(leftmax<rightmax){
+                    //leftmax== rightmax 在那边都一样
+                max+=(leftmax-height[a]);       // leftmax is smaller than rightmax, so the (leftmax-A[a]) water can be stored
+                a++;
+            }
+            else{
+                max+=(rightmax-height[b]);
+                b--;
+            }
+        }
+        return max;
+    }
