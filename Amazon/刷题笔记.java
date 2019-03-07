3eae81bc08a42ffbca09a1411cbe666e8a3f380a
fer <lz1504@nyu.edu>
2019-03-07 14:42:01 -0500

    Update 刷题笔记.java

--- Amazon/刷题笔记.java
+++ Amazon/刷题笔记.java
@@ -3,5 +3,11 @@ trapping rain water(也可以使用单调栈)
 都可以使用 短板原理 比较 比较较短一边 再向一边移动 
 Largest Rectangle in Histogram 也可以单调栈 tbd
 
+#5longest palidrome substring  从中间向两边扩展 并取 low index 和 maxLen 最后返回substring  ababb -> aba 或者 bab
+
+#3longest substring without repeating characets abcabcbb -> abc
+
+
+
 
 
