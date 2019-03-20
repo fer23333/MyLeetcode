f468adb1f0fd89ab40616d5a7b7c9127d5ed03d8
fer <lz1504@nyu.edu>
2019-03-20 19:36:05 -0400

    Update 刷题笔记.java

--- 刷题笔记.java
+++ 刷题笔记.java
@@ -1,6 +1,7 @@
 3/19/2019
 297serilize and deserilize, dfs preorder 和 bfs 的解法
 stringbuilder 和string 在recursion中的区别 
+https://github.com/fer23333/MyLeetcode/blob/master/Tree/StringBuilder%E5%92%8CString%E7%9A%84%E5%8C%BA%E5%88%AB.java
 
 3/20/2019
 199. Binary Tree Right Side View,  bfs + dfs
