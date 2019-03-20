5e0e86d723a3102382f70a8e43dfea3f35e8d0e6
fer <lz1504@nyu.edu>
2019-03-20 19:34:47 -0400

    Update 刷题笔记.java

--- 刷题笔记.java
+++ 刷题笔记.java
@@ -1,2 +1,6 @@
-3/9/2019
-297serilize and deserilize
+3/19/2019
+297serilize and deserilize, dfs preorder 和 bfs 的解法
+stringbuilder 和string 在recursion中的区别 
+
+3/20/2019
+199. Binary Tree Right Side View,  bfs + dfs
