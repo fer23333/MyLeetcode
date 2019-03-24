2bdc3359e430b59496fc6cc9ae6ae19548a7a111
fer <lz1504@nyu.edu>
2019-03-24 16:06:54 -0400

    Update #22. Generate Parentheses.java

--- Parentheses/#22. Generate Parentheses.java
+++ Parentheses/#22. Generate Parentheses.java
@@ -1,4 +1,20 @@
-    public List<String> generateParenthesis(int n) {
+// time O(4^n/ sqrt(n) )Each valid sequence has at most n steps during the backtracking procedure.
+// space O(4^n/ sqrt(n) )as described above, and using O(n) space to store the sequence. 
+
+//this is the n-th Catalan number which is bounded asymptotically by 4^n/ (sqrt(n)*n)
+// 卡特兰数还有那些应用呢?
+
+// 1.括号化 ()() || (()) 这样子滴
+
+// 2.出栈次序(above)
+
+// 3.凸多边形三角划分
+
+// 4.给定结点组成二叉搜索树
+
+// 5.n对括号正确匹配次数
+
+public List<String> generateParenthesis(int n) {
         if(n < 1){
             return new ArrayList<>();
         }
