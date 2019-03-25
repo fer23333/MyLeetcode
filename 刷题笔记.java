497ba6bc74c7d856ce135525bd226d7b0a3d32ae
fer <lz1504@nyu.edu>
2019-03-24 21:26:06 -0400

    Update 刷题笔记.java

--- 刷题笔记.java
+++ 刷题笔记.java
@@ -6,3 +6,20 @@ stringbuilder 和string 在recursion中的区别  https://github.com/fer23333/My
 
 3/20/2019
 199. Binary Tree Right Side View,  bfs + dfs(使用depth 和 res.size() == depth 时才输出)
+  
+3/23
+703 kth largest in data stream  using min heap
+114 flatten binary tree to linkedlist
+116 Populating Next Right Pointers in Each Node  完全二叉树
+117.Populating Next Right Pointers in Each Node II 并非完整二叉树 可以套用 去run116  在每一次层新建dummy node 并连接 
+307 Range sum query- mutable segement tree 在intial 过程建立tree 并完成 update 和sum  左子树为偶数 右子树为奇数 并保持 ij 为一左一右
+
+3/24
+921. Minimum Add to Make Parentheses Valid 栈 如果遇到'(' 推入栈, 如果遇到')' 而且栈不为空 pop, 如果为空 counter++, 最后counter加上栈的size. 
+也可以使用两个变量代替栈 减少空间复杂度
+22. Generate Parentheses 使用backtacking 设置 int open close max 如果open < close 就不是valid, 最后substring.length == max时 return
+301. Remove Invalid Parentheses Time:使用bfs n*2^n, dfs 2^n
+
+
+
+
