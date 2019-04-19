3efe697cd11bbd87123cb1ce86190eef84719789
fer <lz1504@nyu.edu>
2019-04-18 21:21:59 -0400

    Update #111MinDepthTree.java

--- Tree/#111MinDepthTree.java
+++ Tree/#111MinDepthTree.java
@@ -9,3 +9,33 @@
         }
         return Math.min(left,right) + 1;
     }
+
+//bfs
+public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;    
+        }
+        if(root.left == null && root.right == null){
+            return 1;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            depth++;
+            for(int i = 0; i< size; i++){
+                TreeNode cur = queue.poll();
+                if(cur.left != null){
+                    queue.offer(cur.left);
+                }
+                if(cur.right != null){
+                    queue.offer(cur.right);
+                }
+                if(cur.left == null && cur.right == null){
+                    return depth;
+                }
+            }
+        }
+        return depth;
+    }
