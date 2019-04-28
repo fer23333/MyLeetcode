dd9ea0f7cb97db1790ab102d5d6bd70b930dcc02
fer <lz1504@nyu.edu>
2019-04-27 20:23:19 -0400

    Update #658. Find K Closest Elements.java

--- BinarySearch/#658. Find K Closest Elements.java
+++ BinarySearch/#658. Find K Closest Elements.java
@@ -1,3 +1,11 @@
+//寻找最左的左bound o -> n-k
+//0 ... mid...mid+k...n-k
+//如果 x > a[mid], 左边界仍然可能在mid左边
+// 如果a[mid+k] 比a[mid] 更接近 左边界最左一定在mid+1 的位置
+//其他情况都是 right = mid
+
+//不能用绝对值 因为 112222233, x=3,k=2 此时不满足
+
 public List<Integer> findClosestElements(int[] arr, int k, int x) {
         if(arr == null || arr.length == 0){
             return new ArrayList<>();
