9f06d1c7414af044c9dbb4fbbd3900c54ff48c92
fer <lz1504@nyu.edu>
2019-05-01 00:14:54 -0400

    Update #48RotateImage.java

--- Matrix/#48RotateImage.java
+++ Matrix/#48RotateImage.java
@@ -1,46 +1,48 @@
-123
-456
-789
 
-//j要小于i 不然转置两遍就回到原地
-矩阵的转置:
-147
-258
-369
 
-顺时针旋转90度 且要求 inplace:
-741
-852
-963
-
-所以只需要转置在移位即刻
-    public void rotate(int[][] matrix) {
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
+public void rotate(int[][] matrix) {
         if(matrix == null || matrix.length == 0){
             return;
-        }    
-
-        int row =matrix.length;
-        int col =matrix[0].length;
-        //matrix transpose first
-        //j要小于i 不然转置两遍就回到原地
-        for(int i =0; i< row; i++){
-            for(int j =0; j< i; j++){
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // 上下行交换
+        for(int i=0; i< m/2; i++){
+            for(int j=0; j< n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[m-1-i][j];
+                matrix[m-1-i][j] = temp;
+            }
+        }
+        //逆对角线交换
+        for(int i=0; i< m; i++){
+            for(int j= i; j< n; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-        
-        for(int i=0; i< row; i++){
-            int low =0;
-            int high = col-1;
-            while(low < high){
-                int temp = matrix[i][low];
-                matrix[i][low] = matrix[i][high];
-                matrix[i][high] = temp;
-                low++;
-                high--;
-            }
-        }
-        
     }
+
+/*
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+*/
+左右
+void anti_rotate(vector<vector<int> > &matrix) {
+    for (auto vi : matrix) reverse(vi.begin(), vi.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+}
