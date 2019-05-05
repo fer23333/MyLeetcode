82c6c6817ad1932cd50e4d36696ae94f02f2efd7
fer <lz1504@nyu.edu>
2019-05-04 23:48:57 -0400

    Update 50. Pow(x, n).java

--- Recursion/50. Pow(x, n).java
+++ Recursion/50. Pow(x, n).java
@@ -1,5 +1,6 @@
 //先存下half的值 后来return就不用递归到底 2^4 -> 2^2 * 2^2 -> 2^1
 //最后还有一种case n为负数 就为half*half/x
+//T(n) = T(n/2) + O(1) => O(logn)
 
 public double myPow(double x, int n) {
         if(n == 0 ){
