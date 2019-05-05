c88689e7014874c6e4240ccb1859fd61dba821b5
fer <lz1504@nyu.edu>
2019-05-04 23:39:39 -0400

    Update 50. Pow(x, n).java

--- Recursion/50. Pow(x, n).java
+++ Recursion/50. Pow(x, n).java
@@ -8,11 +8,23 @@ public double myPow(double x, int n) {
         
         double half = myPow(x, n/2);
         if(n % 2 == 0){
+			//偶数
             return half*half;
         }
         if(n >0){
+			//奇数
             return x* half * half;
         }
+	//负数
         return half*half/x;
         
     }
+
+//testcase: 2.1000 3
+half = (2,1, 3)   => half = 2.1, return 2.1*2.1*2.1
+|
+half = (2.1, 1)-> half(2.1, 0)
+|
+half = 1
+return 2.1 *1
+       
