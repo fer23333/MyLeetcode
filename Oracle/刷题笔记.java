3a2ac03f1829bcd582b1629194efd308b98902d3
fer <lz1504@nyu.edu>
2019-05-08 17:12:14 -0400

    Update 刷题笔记.java

--- Oracle/刷题笔记.java
+++ Oracle/刷题笔记.java
@@ -1,5 +1,5 @@
 1.valid parentheses 可以先写几个test case 在写方法
 2.reverse linkedlist 要求 one pass and in place
-3.word square 变种 但是输入只有4个词 所以可以暴力解 4!个组合
+3.word square 变种 但是输入只有4个词 所以可以暴力解 4!个组合, 可以说用dfs+trie tree
 
 
