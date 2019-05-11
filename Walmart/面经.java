53c92a461942d9c80ea83ce2533b50480e8846b3
fer <lz1504@nyu.edu>
2019-05-11 17:44:33 -0400

    Create 面经.java

---
+++ Walmart/面经.java
@@ -0,0 +1,7 @@
+Before going to on-site interview, there was a homework designing Drone Delivery System. On the onsite interview, first round is manager interview, second round is Tech interview, which let me explain the Drone Delivery System.
+Instead of discuss with you, the manager challenge you a lot, with aggressive attitude.
+The design part, the design granularity is very unclear, which make the whole process very dizzy.
+
+Q1: Describe a project Q2: Explain your drone delivery code. Q3: How to add a battery function to your designed drone.
+
+Q1: Prepare resume Q2: Explain, which is easy using PriorityQueue Q3: Use OOD to add a battery function
