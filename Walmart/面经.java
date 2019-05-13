194404e42b4608f9ed7405f41eee6395c30ebafe
fer <lz1504@nyu.edu>
2019-05-12 20:50:34 -0400

    Update 面经.java

--- Walmart/面经.java
+++ Walmart/面经.java
@@ -5,3 +5,7 @@ The design part, the design granularity is very unclear, which make the whole pr
 Q1: Describe a project Q2: Explain your drone delivery code. Q3: How to add a battery function to your designed drone.
 
 Q1: Prepare resume Q2: Explain, which is easy using PriorityQueue Q3: Use OOD to add a battery function
+
+
+第一轮面经：简单问了一下基本信息，怎么做quicksort，解释了一下RESTful API 等等。. check 1point3acres for more.
+第二轮面经：发了一个assignment，电影院位置分配问题。然后 全程一个小时面试都是看code，一行行看，顺带问一些OOD的东西。不知道有没有后续消息。bless！！
