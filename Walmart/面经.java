8ec6af3057ddc126a85754de72e06bf9d4a7974d
fer <lz1504@nyu.edu>
2019-05-13 20:52:32 -0400

    Update 面经.java

--- Walmart/面经.java
+++ Walmart/面经.java
@@ -11,3 +11,20 @@ Q1: Prepare resume Q2: Explain, which is easy using PriorityQueue Q3: Use OOD to
 第二轮面经：发了一个assignment，电影院位置分配问题。然后 全程一个小时面试都是看code，一行行看，顺带问一些OOD的东西。不知道有没有后续消息。bless！！
 
 https://github.com/OwenYing/Walmart-labs-Drone-Delivery
+
+
+二面是两个面试官一起面的，其中一个是第一轮screen我的面试官。
+8/30发了一个简单的问题描述，写一个电影院安排座位的implementaion，9/4面试的时候讲，所以大概一周时间。但周末都在弄Amazon，所以其实我只写了昨天一天………………（一晚上……）
+需要screen sharing，所以需要zoom的客户端，browser是不行的，所以如果有用zoom面试的同学提前安装好……
+
+基本就是我开着intellij，首先讲了一下题目背景和要求，然后go over我的代码，看得还是很细的，一共讲了一个小时。感觉基本每行都看了（因为代码本身也不多）。
+然后也看了我写的test，现场要求改一下input file跑一下看结果，结果虽然没bug，但是我的代码输出就有点不太好，不是很as expected。于是他们就问我可以如何避免出现这个问题，我说，多test几个case...他们也指出我的test可以更多。
+其实我事先知道会有这方面的concern，但是时间不够也懒得改了，本来我都不想面了来着…………
+
+总之这种面试只要是自己写的code就没什么大问题，如果是想去的公司就尽量test全面一点，多改进project structure, oo design什么的。
+
+
+二面是一个Take Home Challenge, 一周写代码，店面讲思路， 问我时间复杂度优化，貌似不满意这个
+已挂，move on
+
+https://www.1point3acres.com/bbs/interview/software-engineer-457077.html
